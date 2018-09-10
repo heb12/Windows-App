b9bc87a4b9c9569ead02d79542fb7463055ef294
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-09-10 17:34:57 -0400

    Update getVerse.js

--- getVerse.js
+++ getVerse.js
@@ -10,5 +10,10 @@ var chapter = Number(data[i].split(":")[0]);
 var verse = Number(data[i].split(":")[1]);
 var obj = new Object();
 obj.verse = bible[book][chapter-1][verse-1];
+  var cc = "";
+  for(var a=0; a<bible[book][chapter-1].length; a++) {
+  cc+=a+1+") "+bible[book][chapter-1][a];
+  }
+  obj.chapter = cc;
   return obj;
 }
