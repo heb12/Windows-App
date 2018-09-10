fa92d90d06ce4a38163e36f8b7400cc5acd065f0
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-09-10 17:57:32 -0400

    Create fromJSON.js

---
+++ fromJSON.js
@@ -0,0 +1,1 @@
+  var bible = JSON.parse('{"Genesis":[["In the beginning God created the heavens and the earth.","The earth was without form, and void; and darkness was on the face of the deep. And the Spirit of God was hovering over the face of the waters."]]}');
